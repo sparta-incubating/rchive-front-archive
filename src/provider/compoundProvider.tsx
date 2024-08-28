5391dbf14d5a75b0139eafeed44a9f848a398267
yeolsss <yeol8810@gmail.com>
2024-08-28 14:39:17 +0900

    initsetting

--- src/provider/compoundProvider.tsx
+++ src/provider/compoundProvider.tsx
@@ -1,25 +1,24 @@
 import { PropsWithChildren } from 'react';
 import StoreProvider from '@/provider/storeProvider';
 import TanstackQueryProvider from '@/provider/tanstackQueryProvider/TanstackQueryProvider';
-import SetAuthInfo from '@/utils/auth/setAuthInfo';
-import { auth } from '@/auth';
 import NextAuthProvider from '@/provider/nextAuthProvider/nextAuthProvider';
 
 const CompoundProvider = async ({ children }: PropsWithChildren) => {
-  const session = await auth();
+  // login 완료되면 주석 해제하세요.
+  /*const session = await auth();
   const accessToken = session?.user.accessToken || '';
   const trackName = session?.user.trackName || '';
   const trackRole = session?.user.trackRole || 'USER';
-  const period = String(session?.user.loginPeriod) || '';
+  const period = String(session?.user.loginPeriod) || '';*/
   return (
     <StoreProvider>
       <TanstackQueryProvider>
-        <SetAuthInfo
+        {/*<SetAuthInfo
           accessToken={accessToken}
           trackName={trackName}
           trackRole={trackRole}
           period={String(period)}
-        />
+        />*/}
         <NextAuthProvider>{children}</NextAuthProvider>
       </TanstackQueryProvider>
     </StoreProvider>
