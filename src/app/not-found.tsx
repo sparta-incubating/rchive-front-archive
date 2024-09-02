d8dfebd4346fdf93b4750b9ce426a2b9f5701a4a
yeji Sung <dpwl0332@naver.com>
2024-09-03 03:38:47 +0900

    [#5]feat : not-found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import notFound from '@/../public/assets/icons/not-found.svg';
+import Image from 'next/image';
+import MainHeader from '@/components/molecules/mainHeader';
+import Footer from '@/components/molecules/footer';
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="mx-auto flex min-h-screen w-[1152px] flex-col">
+        <MainHeader />
+        <div className="flex flex-grow items-center justify-center">
+          <Image src={notFound} alt="not-found" width={270} height={258} />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
