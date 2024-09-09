f3564fef4474e3fb00479d640a12e20f4661943f
yeji Sung <dpwl0332@naver.com>
2024-09-09 19:33:20 +0900

    [#37] chore : 빌드 테스트

--- src/components/pages/bookMarkPage.tsx
+++ src/components/pages/bookMarkPage.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import React, { useCallback } from 'react';
-import EmptyPage from './emptyPage';
+
 import { useBookmarkQuery } from '@/api/bookmark/useQuery';
 import PostCard from '../molecules/post/postCard';
 import PageNation from '../atoms/pageNation';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { PostContentType } from '@/types/posts.types';
+import SearchInput from '../organisms/searchInput';
+import HelperButton from '../atoms/helperButton';
 
 const BookMarkPage = () => {
   const { bookmarkList, isPending, isError } = useBookmarkQuery();
@@ -56,35 +58,46 @@ const BookMarkPage = () => {
     return <div>오류발생</div>;
   }
 
-  const handleAllItem = () => {};
-
   return (
-    <div className="mx-auto flex w-full flex-col items-center justify-center gap-[40px] bg-gray-50">
-      <br />
-      <p onClick={handleAllItem}>모두 삭제</p>
-      <div className="relative">
-        <section className="flex flex-col gap-6">
-          {myBookmarkList && myBookmarkList.length > 0 ? (
-            <>
-              <section className="mx-auto mb-14">
-                <div className="grid grid-cols-1 gap-x-6 gap-y-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                  {myBookmarkList.map((item: PostContentType) => (
-                    <PostCard postData={item} key={item.postId} />
-                  ))}
-                </div>
-              </section>
-              <PageNation
-                currentPage={currentPage}
-                totalElements={List.length}
-                size={itemsPerPage}
-                onPageChange={handlePageChange}
-              />
-            </>
-          ) : (
-            <p>리스트가 없음</p>
-          )}
-        </section>
-      </div>
+    // <div className="mx-auto flex w-full flex-col items-center justify-center gap-[40px] bg-gray-50">
+    //   <br />
+    //   <p onClick={handleAllItem}>모두 삭제</p>
+    //   <div className="relative">
+    //     <section className="flex flex-col gap-6">
+
+    //     </section>
+    //   </div>
+    // </div>
+
+    <div className="relative">
+      <SearchInput
+        keyword={keyword}
+        setKeyword={setKeyword}
+        onSearch={handleKeywordSearch}
+        handleSearchClick={handleSearchClick}
+      />
+      <section className="flex flex-col gap-6">
+        {myBookmarkList && myBookmarkList.length > 0 ? (
+          <>
+            <section className="mx-auto mb-14">
+              <div className="grid grid-cols-1 gap-x-6 gap-y-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {myBookmarkList.map((item: PostContentType) => (
+                  <PostCard postData={item} key={item.postId} />
+                ))}
+              </div>
+            </section>
+            <PageNation
+              currentPage={currentPage}
+              totalElements={List.length}
+              size={itemsPerPage}
+              onPageChange={handlePageChange}
+            />
+          </>
+        ) : (
+          <p>리스트가 없음</p>
+        )}
+      </section>
+      <HelperButton />
     </div>
   );
 };
