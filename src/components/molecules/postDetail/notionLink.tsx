d05b536bdef7bd0f37f5b9930b0e9fd53d8a814a
yeolsss <yeol8810@gmail.com>
2024-09-13 08:47:22 +0900

    update : notion 안내 문구 워딩 변경

--- src/components/molecules/postDetail/notionLink.tsx
+++ src/components/molecules/postDetail/notionLink.tsx
@@ -4,7 +4,7 @@ const NotionLink = ({ notionLink }: { notionLink: string }) => {
   return (
     <div className="flex h-[58px] w-[814px] items-center justify-center gap-2.5 rounded-[8px] border border-blue-100 bg-blue-55">
       <p className="text-md font-medium">
-        첨부된 영상이 보이지 않을 경우 노션에서 확인해주세요!
+        첨부된 영상이 실행되지 않을 경우 노션에서 실행해주세요!
       </p>
       <a href={notionLink} target="_blank">
         <div className="flex">
